726a622cb9be70e641e4279fe1f768f7067920d9
Saltflsh <598155869@qq.com>
2020-08-17 00:04:25 +0800

    fix api

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,21 @@
 import axios from 'axios'
 
+// set different base URL between development and production environment
 if (process.env.NODE_ENV === 'development'){
-    axios.defaults.baseURL = 'http://api.github.com'
-    //axios.defaults.baseURL = 'http://localhost:3000'
+    //axios.defaults.baseURL = 'http://api.github.com'
+    axios.defaults.baseURL = 'http://localhost:3000'
 } else if(process.env.NODE_ENV === 'production'){
-    axios.defaults.baseURL = 'https://api.com'
+    axios.defaults.baseURL = process.env.VUE_APP_API_ROOT
 }
 
 export function getContributorsData(per_page, page){
     return axios({
         method: 'get',
-        //url: `api?per_page=${per_page}&page=${page}`
-        url: `/repos/pingcap/tidb/contributors?per_page=${per_page}&page=${page}`,
+        url: `api?per_page=${per_page}&page=${page}`,
+        //url: `/repos/pingcap/tidb/contributors?per_page=${per_page}&page=${page}`,
         header: {
                     "Content-Type": "application/json",
-        "Authorization": "bearer 95e43f78c6e92a23303e081c88dc75dcee4dbfd7"
+        //"Authorization": "bearer ******************************"
         }
     })
 }
